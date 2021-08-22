3f348bfef466728abce9aa1bfe2615bab95f1746
IshikaThilomi <68691235+IshikaThilomi@users.noreply.github.com>
2021-08-22 18:33:42 +0530

    Initial-commit

---
+++ frontend/src/Routes.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//signin and signup 
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+
+//role routes
+import UserRoutes from "./auth/helper/AdminRoutes";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
+
+//management dashboards
+import HomeAfterLogin from "./core/Home";
+
+//profiles
+import userProfile from "./user/userProfile";
+
+//Accomodations
+import Accomodations from './user/Events';
+import AddNewVenueType from "./admin/AddNewVenueType";
+import ManageRooms from "./admin/ManageVenues";
+import UpdateRooms from "./admin/UpdateRooms";
+
+// import ApprovedReseachPaper from "./admin/ApprovedResearchPapers";
+;
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login-home" exact component={HomeAfterLogin} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/" exact component={Signin} />
+
+        <UserRoutes path="/user/profile" exact component={userProfile}/>
+
+        <UserRoutes path="/admin/create/roomtype" exact component={AddNewVenueType}/>
+
+        <UserRoutes path="/admin/manage-venues" exact component={ManageRooms} />
+        
+        <UserRoutes path="/admin/product/update/:productId" exact component={UpdateRooms}/>
+
+        <UserRoutes path="/admin/events-task" exact component={Accomodations}/>
+
+        
+        {/* <UserRoutes path="/available-rooms" exact component={ApprovedReseachPaper} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
