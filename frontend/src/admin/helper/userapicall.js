f3782d631532dee0a71566bd961515d829b6db49
IshikaThilomi <68691235+IshikaThilomi@users.noreply.github.com>
2021-09-30 01:45:01 +0530

    Code Refactoring

--- frontend/src/admin/helper/userapicall.js
+++ frontend/src/admin/helper/userapicall.js
@@ -202,3 +202,73 @@ export const updateProduct = (productId, userId, token, product) => {
     .catch(err => console.log(err));
 };
 
+export const AddNewRoomType = (userId, token, product) => {
+  return fetch(`${API}/new-room-type/add/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+/////////// Display all room types in the table view //////////////////
+export const getAllRoomTypes = () => {
+  return fetch(`${API}/room-types`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+
+/////////////delete a room type from the list///////////////////////
+export const deleteRoomType = (productId, userId, token) => {
+    return fetch(`${API}/room/${productId}/${userId}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
+
+
+
+//not modified
+
+export const getProduct = productId => {
+    return fetch(`${API}/room/${productId}`, {
+      method: "GET"
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
+
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/room/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
